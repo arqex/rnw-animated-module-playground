e55af7c12b649724bf17f851afff67f333748f26
Javier Marquez <javi@arqex.com>
2019-03-20 09:43:25 +0100

    Adds some code for the animator

--- src/nativeAnimatedModule/Animator.js
+++ src/nativeAnimatedModule/Animator.js
@@ -1,26 +1,35 @@
 const Animator = {
 	createAnimation: function( definitions ){
-		let jsAnimations = [];
-		definitions.forEach( def => {
-			jsAnimations.push( 
-				new Animation( new KeyframeEffect( def.view, ))
-			})
-		})
+		let webAnimations = createWebAnimations( definitions );
 		
 		return {
 			start: function(){
-
+				webAnimations.forEach( ani => ani.start() )
 			},
-			stop: function(){
-
+			stop: function () {
+				webAnimations.forEach(ani => ani.pause())
 			},
 			reset: function(){
-
+				this.stop()
+				webAnimations = createWebAnimations(definitions)
 			}
 		}
 	}
 }
 
-function createWebAnimation( definition ){
-
+function createWebAnimations( definitions ){
+	let webAnimations = definitions.map( def => {
+		let keyFrames = {}
+		for( let property in def.style ){
+			if( property === 'transform' ){
+				let transform = def.style.transform
+				for( let t in transform ){
+					
+				}
+			}
+			else {
+				keyFrames[ property ] = def.style[property].values;
+			}
+		}
+	})
 }
