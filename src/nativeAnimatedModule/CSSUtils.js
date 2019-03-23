e39be79697e80a2f04e9ba7d7cd9da06a5327d7b
Javier Marquez <javi@arqex.com>
2019-03-23 12:17:41 +0100

    CSS class makes easy to update individual animations

--- src/nativeAnimatedModule/CSSUtils.js
+++ src/nativeAnimatedModule/CSSUtils.js
@@ -1,30 +1,83 @@
 let ids = 0;
 
+const SEPARATOR = ', '
+
 class CSSAnimation {
 	constructor( view, duration, iterations ){
-		super( super )
-		this.id = 'nam_' + (ids++)
-		this.view = view
-		this.duration = duration
-		this.iterations = iterations
+		super()
+		
+		this.view = view;
+		this.animation = {
+			name: 'nam_' + (ids++),
+			duration,
+			timing: 'linear',
+			delay: '0',
+			iterations: iterations,
+			direction: 'normal',
+			fill: 'both',
+			playState: 'initial'
+		}
+
+		this._addAnimationToView()
 	}
 
 	start(){
-		let style = this.view.style
-		let animation = this.getStyleAnimation();
-		if( style.animation ){
-			style.animation += ', ' + this.getStyleAnimation()
+		this._updateAnimation({playState: 'running'})
+	}
+
+	stop() {
+		this._updateAnimation({ playState: 'paused' })
+	}
+
+	reset() {
+		this._updateAnimation({ playState: 'initial' })
+	}
+
+	_addAnimationToView(){
+		this._updateAnimation();
+	}
+
+	_removeAnimationFromView(){
+		let animations = this._getViewAnimations()
+		if( animations.currentIndex !== false ){
+			animations.list.splice( animations.currentIndex, 1 )
+			this._setViewAnimations(animations.list)
+		}		
+	}
+
+	_updateAnimation( update = {} ){
+		let nextAnimation = this._getCSSAnimation({ ...this.animation, update })
+		let animations = this._getViewAnimations()
+
+		if( animations.currentIndex !== false ){
+			animations.list[ animations.currentIndex ] = nextAnimation
 		}
 		else {
-			style.animation = this.getStyleAnimation()
+			animations.list.push( nextAnimation )
+		}
+
+		this._setViewAnimations( animations.list )
+	}
+
+	_getViewAnimations(){
+		let list = (this.view.style.animation || '').split(/\s*,\s*/)
+		let i = 0;
+		let index = false;
+
+		while( index === false && i < list.length ){
+			if( list[i].indexOf( this.animation.name ) === 0 ){
+				index = i
+			}
+			i++;
+		}
+
+		return {
+			list: list,
+			currentIndex: index
 		}
 	}
 
-	stop(){
-
-	}
-
-	reset(){
-
+	_setViewAnimations( list ){
+		this.view.style.animations = list.join(SEPARATOR);
 	}
 }
